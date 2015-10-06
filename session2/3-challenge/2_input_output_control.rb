c26514faa40a0cc154f558bd2cbe1b9a71a640ce
fergus lemon <fergus.lemon@gmail.com>
2015-10-06 18:21:33 +0100

    update session1 & 2

--- session2/3-challenge/2_input_output_control.rb
+++ session2/3-challenge/2_input_output_control.rb
@@ -18,10 +18,20 @@
 # remember you can try your program out with              $ ruby 2_input_output_control.rb
 # and when you think it is correct, you can test it with  $ rake 2:2
 
-def hi_hi_goodbye
-  # your code here
-
+def input
+  puts "'Please enter a number or the word 'bye'"
 end
+
+def hi_hi_goodbye
+  input
+  while (line = gets) && (line !~ /bye/) 
+    line.to_i.times { print 'hi ' }
+    puts
+    input
+  end
+  puts "Goodbye!"
+end
+
 
 
 
--- session2/3-challenge/8_array.rb
+++ session2/3-challenge/8_array.rb
@@ -6,3 +6,9 @@
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
 
+def three_in_a_row (elements)
+	elements.each_cons 3 do |a, b, c|
+    return true if a == b && b == c
+  end
+  false
+end
